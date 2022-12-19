2f98982154dfd7677adf9c1857ecaea93247f56b
petrenkosofya <Sofi20102005@gmail.com>
2022-12-19 13:40:48 +0500

    skip updates

--- src/Bot/Geter.py
+++ src/Bot/Geter.py
@@ -8,22 +8,26 @@ image_generation = StableDiffusion()
 
 
 def get_media(text):
-    media = types.MediaGroup()
+    media = list()
+    media_r = types.MediaGroup()
     emb_true = image_generation.text_embedding(text)
     print("embedding getted")
     image_true = image_generation.generate_image(emb_true)
     print("first picture generated")
-    media.attach_photo(image_true)
+    media.append(image_true)
     for i in range(2):
         new_text = edit_text_bert(text, 1)
         emb = image_generation.text_embedding(new_text)
         image = image_generation.generate_image(emb)
-        media.attach_photo(image)
+        media.append(image)
     for i in range(2):
         emb = image_generation.text_embedding(text)
         emb = edit_text_latent(emb, 7)
         image = image_generation.generate_image(emb)
-        media.attach_photo(image)
+        media.append(image)
     pos_who_must_replace = randint(0, 5)
     media[0], media[pos_who_must_replace] = media[pos_who_must_replace], media[0]
+    for i in range(0, 5):
+        media_r.attach_photo(media[i])
+    print("replaced")
     return [media, pos_who_must_replace + 1, text]
