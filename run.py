6f10eec2eceb283ec4b950944f40184f2be2c01b
petrenkosofya <Sofi20102005@gmail.com>
2022-12-19 16:25:45 +0500

    skip updates

--- run.py
+++ run.py
@@ -26,7 +26,7 @@ async def GenPic(msg: types.Message):
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
     buttons = ["1", "2", "3", "4", "5"]
     keyboard.add(*buttons)
-    text = generate_promt()
+    text = generate_promt("surreal drawings")
     print("Promt generated: " + text)
     data = get_media(text)
     print("Media generated")
