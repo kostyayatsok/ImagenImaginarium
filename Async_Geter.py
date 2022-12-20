4f4c5dc0b5c8174f83ddf0003e73937b0f41ff91
MucTeP-PrograMMucTeP <dima.semenov.100500@icloud.com>
2022-12-20 10:06:32 +0300

    Fixed bugs

--- Async_Geter.py
+++ Async_Geter.py
@@ -6,7 +6,6 @@ from src.text_editing.bert_text_editor import edit_text_bert
 from src.text_editing.edit_text_latent import edit_text_latent
 from src.text_generation.gpt2 import generate_promt
 
-
 LABEL = 1
 PICTURE_NUMBER = 1
 IMAGES_PATH = "Images"
@@ -15,6 +14,7 @@ TABLE = pd.read_csv(DATABASE_PATH)
 
 image_generation = StableDiffusion()
 os.makedirs(IMAGES_PATH, exist_ok=True)
+
 
 def get_picture_name():
     global PICTURE_NUMBER
@@ -46,7 +46,7 @@ def add_media(text=""):
     emb_true = image_generation.text_embedding(text)
     image_true = image_generation.generate_image(emb_true)
     picture_name = get_picture_name()
-    img_path = IMAGES_PATH + '\\' + picture_name
+    img_path = IMAGES_PATH + '/' + picture_name
     add_table_row(img_path, LABEL, text, "True")
     image_true.save(img_path)
     for _ in range(2):
@@ -54,7 +54,7 @@ def add_media(text=""):
         emb = image_generation.text_embedding(new_text)
         image = image_generation.generate_image(emb)
         picture_name = get_picture_name()
-        img_path = IMAGES_PATH + '\\' + picture_name
+        img_path = IMAGES_PATH + '/' + picture_name
         add_table_row(img_path, LABEL, new_text, "False")
         image.save(img_path)
     for _ in range(2):
@@ -62,10 +62,10 @@ def add_media(text=""):
         emb = edit_text_latent(emb, 7)
         image = image_generation.generate_image(emb)
         picture_name = get_picture_name()
-        img_path = IMAGES_PATH + '\\' + picture_name
+        img_path = IMAGES_PATH + '/' + picture_name
         add_table_row(img_path, LABEL, "None", "False")
         image.save(img_path)
-    if LABEL % 10 == 0:
+    if LABEL % 1 == 0:
         save()
     LABEL += 1
     return
