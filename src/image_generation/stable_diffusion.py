1db9f70b4b8104aea5419946c1c665cb47be0e51
petrenkosofya <Sofi20102005@gmail.com>
2022-12-22 16:35:35 +0500

    changed promt generation

--- src/image_generation/stable_diffusion.py
+++ src/image_generation/stable_diffusion.py
@@ -41,7 +41,7 @@ class StableDiffusion:
 
         max_length = text_input.input_ids.shape[-1]
         uncond_input = self.tokenizer(
-            ["portrait, anime, nacked, swollen, blurry, out of focus, slanting eyes, deformed, asymmetrical face, gross proportions,"\
+            ["portrait, anime, nacked, text, swollen, blurry, out of focus, slanting eyes, deformed, asymmetrical face, gross proportions,"\
              "photo, missing arms, bad anatomy, disfigured, poorly drawn face, mutation, mutated, extra limb, ugly,"\
              "missing limb, cloned face, floating limbs, disconnected limbs, malformed hands, blur, out of focus,"\
              "long neck, long body, morbid, mutilated, extra fingers, multilated hands, poorly drawn hands, "\
