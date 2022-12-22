cfafe5682dc0b115ea777f0244021842bdcd2036
petrenkosofya <Sofi20102005@gmail.com>
2022-12-22 12:01:27 +0500

    changed promt generation

--- database.py
+++ database.py
@@ -25,7 +25,7 @@ os.makedirs(IMAGES_PATH, exist_ok=True)
 
 def get_picture_name(pic):
     #return f"{IMAGES_PATH}/{DATABASE_NAME}_{LABEL:05d}_{pic:02d}.png"
-    return f"a.png"
+    return "a.png"
 
 
 def add_table_row(img_path, label, text, promt_text, main_picture):
