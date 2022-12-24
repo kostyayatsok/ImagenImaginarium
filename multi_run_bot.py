d69d8623f6a26e3eac770114c5ad5964e4d5b267
kostyayatsok <kielenik@edu.hse.ru>
2022-12-24 10:05:00 +0300

    qwer

--- multi_run_bot.py
+++ multi_run_bot.py
@@ -52,6 +52,8 @@ async def Start(msg: types.Message):
     if msg.chat.id in Gamers:
         if msg.chat.id in Gamers[msg.chat.id].List:
             Gamers[msg.chat.id].List.remove(msg.chat.id)
+        if msg.chat.id in Gamers[msg.chat.id].Gamers:
+            del Gamers[msg.chat.id].Gamers[msg.chat.id]
         cur_Game = Gamers[msg.chat.id]
         del Gamers[msg.chat.id]
         if cur_Game not in Gamers.values():
